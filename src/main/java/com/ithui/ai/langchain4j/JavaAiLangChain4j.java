9f05f96948b77dc71f557ea844088b4f4231b9a1
jihui <jihui@31.com>
2025-04-23 16:04:26 +0800

    SpringBoot集成openapi,ollama,阿里云百炼大模型

---
+++ src/main/java/com/ithui/ai/langchain4j/JavaAiLangChain4j.java
@@ -0,0 +1,12 @@
+package com.ithui.ai.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAiLangChain4j {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAiLangChain4j.class, args);
+    }
+}
