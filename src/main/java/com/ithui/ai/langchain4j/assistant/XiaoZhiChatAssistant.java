3d27ba0caadd6f0fc87beb70b27c450dfc4226d7
jihui <jihui@31.com>
2025-06-02 17:29:45 +0800

    小智助手流式输出

--- src/main/java/com/ithui/ai/langchain4j/assistant/XiaoZhiChatAssistant.java
+++ src/main/java/com/ithui/ai/langchain4j/assistant/XiaoZhiChatAssistant.java
@@ -4,6 +4,7 @@ import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
+import reactor.core.publisher.Flux;
 
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 
@@ -12,7 +13,8 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
  */
 @AiService(
         wiringMode = EXPLICIT,
-        chatModel = "qwenChatModel",
+        /*chatModel = "qwenChatModel",*/
+        streamingChatModel = "qwenStreamingChatModel",
         chatMemoryProvider = "xiaozhiChatMemoryProvider",
         tools = "appointmentTool" ,//tools配置
         contentRetriever = "contentRetrieverXiaozhiPincone" //contentRetriever配置
@@ -20,5 +22,5 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 public interface XiaoZhiChatAssistant {
 
     @SystemMessage(fromResource = "xiaozhi-prompt-template.txt")
-    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+    Flux<String> chat(@MemoryId Long memoryId, @UserMessage String userMessage);
 }
